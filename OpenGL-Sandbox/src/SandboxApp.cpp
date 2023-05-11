42f5944eafb97004fa1eba83069a7c2c90585609
Rogunath, Binit <binitrogunath@hotmail.com>
2023-05-11 22:23:26 +0100

    Completed Episode: Batch Rendering Indices

--- OpenGL-Sandbox/src/SandboxApp.cpp
+++ OpenGL-Sandbox/src/SandboxApp.cpp
@@ -12,6 +12,18 @@ public:
     }
 };
 
+// Force rendering on NVIDIA GPU
+extern "C"
+{
+    __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+}
+
+// Force rendering on AMD GPU
+extern "C"
+{
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
 int main()
 {
     std::unique_ptr<Sandbox> app = std::make_unique<Sandbox>();
--- OpenGL-Sandbox/src/SandboxLayer.cpp
+++ OpenGL-Sandbox/src/SandboxLayer.cpp
@@ -28,17 +28,18 @@ static GLuint LoadTexture(const std::string& path)
 
     // Load image png file
     stbi_set_flip_vertically_on_load(1);
-    auto* pixels = stbi_load(path.c_str(), &width, &height, &bitsPerPixel, STBI_rgb);
+    auto* pixels = stbi_load(path.c_str(), &width, &height, &bitsPerPixel, STBI_rgb_alpha);
     GLuint textureID;
 
-    glCreateTextures(GL_TEXTURE_2D, 1, &textureID);
+    glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_2D, textureID);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
     stbi_image_free(pixels);
 
@@ -54,12 +55,21 @@ void SandboxLayer::OnAttach()
         "assets/shader.glsl.frag"
     ));
 
+    // Enable Blending
+    glEnable(GL_BLEND);
+    // We are defining how open GL is going to blend alpha pixels
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     glUseProgram(m_Shader->GetRendererID());
     const auto loc = glGetUniformLocation(m_Shader->GetRendererID(), "u_Textures");
     int samplers[2] = { 0, 1 };
     glUniform1iv(loc, 2, samplers);
 
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+    const size_t MaxQuadCount = 1000;
+    const size_t MaxVertexCount = MaxQuadCount * 4;
+    const size_t MaxIndexCount = MaxQuadCount * 6;
 
     // Vertex Array
     glCreateVertexArrays(1, &m_QuadVA);
@@ -68,7 +78,7 @@ void SandboxLayer::OnAttach()
     // Vertex Buffer
     glCreateBuffers(1, &m_QuadVB);
     glBindBuffer(GL_ARRAY_BUFFER, m_QuadVB);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 1000, nullptr, GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, MaxVertexCount * sizeof(Vertex), nullptr, GL_DYNAMIC_DRAW);
 
     // Setup Vertex Attribute Pointer - For position
     glEnableVertexArrayAttrib(m_QuadVB, 0);
@@ -86,10 +96,24 @@ void SandboxLayer::OnAttach()
     glEnableVertexArrayAttrib(m_QuadVB, 3);
     glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<const void*>(offsetof(Vertex, TexID)));
 
-    uint32_t indices[] = {
+    /*uint32_t indices[] = {
         0, 1, 2, 2, 3, 0,
         4, 5, 6, 6, 7, 4
-    };
+    };*/
+
+    uint32_t indices[MaxIndexCount];
+    uint32_t offset = 0;
+    for (size_t i = 0; i < MaxIndexCount; i += 6)
+    {
+        indices[i] = offset;
+        indices[i+1] = 1 + offset;
+        indices[i+2] = 2 + offset;
+        indices[i+3] = 2 + offset;
+        indices[i+4] = 3 + offset;
+        indices[i+5] = offset;
+
+        offset += 4;
+    }
 
     // Index Buffer
     glCreateBuffers(1, &m_QuadIB);
@@ -110,35 +134,35 @@ void SandboxLayer::OnEvent(Event& event)
     m_CameraController.OnEvent(event);
 }
 
-static std::array<Vertex, 4> CreateQuad(float x, float y, float textureID)
+static Vertex* CreateQuad(Vertex* target, float x, float y, float textureID)
 {
-    float size = 1.0f;
+    const float size = 1.0f;
 
-    Vertex v0;
-    v0.Position = { x, y, 0.0f };
-    v0.Color = { 0.18f, 0.6f, 0.96f, 1.0f };
-    v0.TexCoords = { 0.0f, 0.0f };
-    v0.TexID = textureID;
+    target->Position = { x, y, 0.0f };
+    target->Color = { 0.18f, 0.6f, 0.96f, 1.0f };
+    target->TexCoords = { 0.0f, 0.0f };
+    target->TexID = textureID;
+    target++;
 
-    Vertex v1;
-    v1.Position = { x + size, y, 0.0f };
-    v1.Color = { 0.18f, 0.6f, 0.96f, 1.0f };
-    v1.TexCoords = { 1.0f, 0.0f };
-    v1.TexID = textureID;
+    target->Position = { x + size, y, 0.0f };
+    target->Color = { 0.18f, 0.6f, 0.96f, 1.0f };
+    target->TexCoords = { 1.0f, 0.0f };
+    target->TexID = textureID;
+    target++;
 
-    Vertex v2;
-    v2.Position = { x + size,  y + size, 0.0f };
-    v2.Color = { 0.18f, 0.6f, 0.96f, 1.0f };
-    v2.TexCoords = { 1.0f, 1.0f };
-    v2.TexID = textureID;
+    target->Position = { x + size,  y + size, 0.0f };
+    target->Color = { 0.18f, 0.6f, 0.96f, 1.0f };
+    target->TexCoords = { 1.0f, 1.0f };
+    target->TexID = textureID;
+    target++;
 
-    Vertex v3;
-    v3.Position = { x,  y + size, 0.0f };
-    v3.Color = { 0.18f, 0.6f, 0.96f, 1.0f };
-    v3.TexCoords = { 0.0f, 1.0f };
-    v3.TexID = textureID;
+    target->Position = { x,  y + size, 0.0f };
+    target->Color = { 0.18f, 0.6f, 0.96f, 1.0f };
+    target->TexCoords = { 0.0f, 1.0f };
+    target->TexID = textureID;
+    target++;
 
-    return { v0, v1,v2,v3};
+    return target;
 }
 
 void SandboxLayer::OnUpdate(Timestep ts)
@@ -159,16 +183,25 @@ void SandboxLayer::OnUpdate(Timestep ts)
          0.5f,  0.5f, 0.0f, 1.0f, 0.93f, 0.24f, 1.0f, 0.0f, 1.0f, 1.0f,
     };*/
 
-    // Set dynamic Vertex Buffer
-    auto q0 = CreateQuad(m_QuadAPosition[0], m_QuadAPosition[1], 0.0f);
-    auto q1 = CreateQuad(m_QuadBPosition[0], m_QuadBPosition[1], 1.0f);
+    uint32_t indexCount = 0;
+    std::array<Vertex, 1000> vertices{};
+    Vertex* buffer = vertices.data();
 
-    Vertex vertices[8];
-    memcpy(vertices, q0.data(), q0.size() * sizeof(Vertex));
-    memcpy(vertices + q0.size(), q1.data(), q1.size() * sizeof(Vertex));
+    // Create a 5 by 5 array of quads
+    for (int x = 0; x < 5; x++)
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            buffer = CreateQuad(buffer, float(x), float(y), float((x + y) % 2));
+            indexCount += 6;
+        }
+    }
+
+    CreateQuad(buffer, m_QuadAPosition[0], m_QuadAPosition[1], 0.0f);
+    indexCount += 6;
 
     glBindBuffer(GL_ARRAY_BUFFER, m_QuadVB);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(Vertex), vertices.data());
 
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -183,7 +216,7 @@ void SandboxLayer::OnUpdate(Timestep ts)
     //m_Shader->SetUniform4fv(m_Shader->GetRendererID(), "u_Color", m_SquareColor);
 
     glBindVertexArray(m_QuadVA);
-    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
+    glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, nullptr);
 }
 
 void SandboxLayer::OnImGuiRender()
